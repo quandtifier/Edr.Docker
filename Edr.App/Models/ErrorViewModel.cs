fee021ad3cdd398e74cf7eb8a8da60dc9475e896
quandtifier <quandtifier@gmail.com>
2022-04-29 12:03:42 -1000

    Added and configured Edr.App

---
+++ Edr.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Edr.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
