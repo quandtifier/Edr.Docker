36c1344dfc31f94c98f8a64123b6c80f0c037453
quandtifier <quandtifier@gmail.com>
2022-04-29 08:56:38 -1000

    PodProcessor configured for serilog

--- Edr.PodProcessor/Program.cs
+++ Edr.PodProcessor/Program.cs
@@ -1,10 +1,36 @@
 using Edr.PodProcessor;
+using Serilog;
+using Serilog.Events;
 
-IHost host = Host.CreateDefaultBuilder(args)
+var assName = typeof(Program).Assembly.GetName().Name;
+Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .Enrich.FromLogContext()
+            .Enrich.WithMachineName()
+            .Enrich.WithProperty("Assembly", assName)
+            .WriteTo.Seq(serverUrl: "http://host.docker.internal:5341")
+            .WriteTo.Console()
+            .CreateLogger();
+try
+{
+    Log.Information("Starting host");
+
+    IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
         services.AddHostedService<Worker>();
     })
+    .UseSerilog()
     .Build();
 
-await host.RunAsync();
+    await host.RunAsync();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
+
