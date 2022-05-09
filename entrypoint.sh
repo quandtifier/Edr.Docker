c4c9716367c5769bcac881acd5500398609cfd5b
quandtifier <quandtifier@gmail.com>
2022-05-09 08:55:51 -1000

    all messed up

--- entrypoint.sh
+++
@@ -1,2 +0,0 @@
-﻿#start SQL Server, start script to create db and import data, start app
-/opt/mssql/bin/sqlservr & ./setup.sh & sleep infinity & wait
--- sql/setup.sh
+++
@@ -1,3 +0,0 @@
-# Wait for SQL server to be started then run the sql script
-./wait-for-it.sh sql_in_dc:1433 --timeout=0 --strict -- sleep 5s && \
-/opt/mssql-tools/bin/sqlcmd -S localhost  -U sa -P "$SA_PASSWORD"
